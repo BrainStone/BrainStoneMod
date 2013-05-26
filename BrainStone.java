b5f7835579de810e30f2dcffeb91d0119176e9f7
BrainStone <yannick@tedworld.de>
2013-05-26 15:01:54 +0200

    v2.28.14 BETA
    - Improved update note

--- BrainStone.java
+++ BrainStone.java
@@ -89,7 +89,7 @@ import cpw.mods.fml.relauncher.SideOnly;
  * 
  * @author Yannick Schinko (alias The_BrainStone)
  */
-@Mod(modid = "BrainStoneMod", name = "Brain Stone Mod", version = "v2.28.13 BETA")
+@Mod(modid = "BrainStoneMod", name = "Brain Stone Mod", version = "v2.28.14 BETA")
 @NetworkMod(clientSideRequired = true, serverSideRequired = true, channels = {
 		"BSM", // generic Packet
 		"BSM.TEBBSTS", // TileEntityBlockBrainStoneTrigger Server Packet
@@ -485,19 +485,25 @@ public class BrainStone {
 		switch (updateNotification) {
 		case 0:
 			if (isHigherVersion(version, releaseVersion)) {
-				sendToPlayer("§aA new Version of the BSM is available!\n§l§c========== "
-						+ releaseVersion + " ==========");
+				sendToPlayer("§aA new Version of the BSM is available!\n§l§c========== &4"
+						+ releaseVersion
+						+ "§c =========="
+						+ "§9Download it at §1http://adf.ly/2002096/release §9or §1https://github.com/BrainStone/brainstone§9!");
 			}
 
 			break;
 		case 1:
 			if (isHigherVersion(version, releaseVersion)
 					&& !isHigherVersion(releaseVersion, recommendedVersion)) {
-				sendToPlayer("§aA new Version of the BSM is available!\n§l§c========== "
-						+ releaseVersion + " ==========");
+				sendToPlayer("§aA new Version of the BSM is available!\n§l§c========== &4"
+						+ releaseVersion
+						+ "§c =========="
+						+ "§9Download it at §1http://adf.ly/2002096/release §9or §1https://github.com/BrainStone/brainstone§9!");
 			} else if (isHigherVersion(version, recommendedVersion)) {
-				sendToPlayer("§aA new recommended DEV Version of the BSM is available!\n§l§c========== "
-						+ recommendedVersion + " ==========");
+				sendToPlayer("§aA new recommended DEV Version of the BSM is available!\n§l§c========== &4"
+						+ recommendedVersion
+						+ "§c =========="
+						+ "§9Download it at §1http://adf.ly/2002096/recommended §9or §1https://github.com/BrainStone/brainstone§9!");
 			}
 
 			break;
@@ -505,15 +511,21 @@ public class BrainStone {
 			if (isHigherVersion(version, releaseVersion)
 					&& !isHigherVersion(releaseVersion, recommendedVersion)
 					&& !isHigherVersion(releaseVersion, latestVersion)) {
-				sendToPlayer("§aA new Version of the BSM is available!\n§l§c========== "
-						+ releaseVersion + " ==========");
+				sendToPlayer("§aA new Version of the BSM is available!\n§l§c========== &4"
+						+ releaseVersion
+						+ "§c =========="
+						+ "§9Download it at §1http://adf.ly/2002096/release §9or §1https://github.com/BrainStone/brainstone§9!");
 			} else if (isHigherVersion(version, recommendedVersion)
 					&& !isHigherVersion(recommendedVersion, latestVersion)) {
-				sendToPlayer("§aA new recommended DEV Version of the BSM is available!\n§l§c========== "
-						+ recommendedVersion + " ==========");
+				sendToPlayer("§aA new recommended DEV Version of the BSM is available!\n§l§c========== &4"
+						+ recommendedVersion
+						+ "§c =========="
+						+ "§9Download it at §1http://adf.ly/2002096/recommended §9or §1https://github.com/BrainStone/brainstone§9!");
 			} else if (isHigherVersion(version, latestVersion)) {
-				sendToPlayer("§aA new DEV Version of the BSM is available!\n§l§c========== "
-						+ latestVersion + " ==========");
+				sendToPlayer("§aA new DEV Version of the BSM is available!\n§l§c========== &4"
+						+ latestVersion
+						+ "§c =========="
+						+ "§9Download it at §1http://adf.ly/2002096/latest §9or §1https://github.com/BrainStone/brainstone§9!");
 			}
 
 			break;
