b86f62039b15d50fdc23a6b4f3ad5a2fa45a9541
BrainStone <yannick@tedworld.de>
2013-07-11 00:38:13 +0200

    v2.31.3 BETA
    - Added mcmod.info

--- brainstonemod/BrainStone.java
+++ brainstonemod/BrainStone.java
@@ -84,7 +84,7 @@ import cpw.mods.fml.relauncher.SideOnly;
  * 
  * @author Yannick Schinko (alias The_BrainStone)
  */
-@Mod(modid = "BrainStoneMod", name = "Brain Stone Mod", version = "v2.31.2 BETA")
+@Mod(modid = "BrainStoneMod", name = "Brain Stone Mod", version = "v2.31.3 BETA")
 @NetworkMod(clientSideRequired = true, serverSideRequired = true, channels = {
 		"BSM", // generic Packet
 		"BSM.TEBBSTS", // TileEntityBlockBrainStoneTrigger Server Packet
