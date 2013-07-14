69ca205a6916cb5d77b5489ca3aa8ba9b8c2cb2a
BrainStone <yannick@tedworld.de>
2013-07-14 03:43:25 +0200

    v2.32.79 BETA release
    - Update to 1.6.2
    - Removed coal briquette (Minecraft has a coal block)
    - Remake of almost all textures
    - Mobs wearing full BrainStone armor will not be affected by the
    PulsatingBrainStone
    - Some small bugfixes
    - Some small improvements

--- brainstonemod/BrainStone.java
+++ brainstonemod/BrainStone.java
@@ -83,7 +83,7 @@ import cpw.mods.fml.relauncher.SideOnly;
  * 
  * @author Yannick Schinko (alias The_BrainStone)
  */
-@Mod(modid = "BrainStoneMod", name = "Brain Stone Mod", version = "v2.32.78 BETA")
+@Mod(modid = "BrainStoneMod", name = "Brain Stone Mod", version = "v2.32.79 BETA release")
 @NetworkMod(clientSideRequired = true, serverSideRequired = true, channels = {
 		"BSM", // generic Packet
 		"BSM.TEBBSTS", // TileEntityBlockBrainStoneTrigger Server Packet
