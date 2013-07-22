26b1b10db91482937f1aa470bc337a996657e2e5
BrainStone <yannick@tedworld.de>
2013-07-23 00:13:33 +0200

    v2.34.116 BETA
    - Forgot a line (fixed it)

--- brainstonemod/BrainStone.java
+++ brainstonemod/BrainStone.java
@@ -86,7 +86,7 @@ import cpw.mods.fml.relauncher.SideOnly;
  * 
  * @author Yannick Schinko (alias The_BrainStone)
  */
-@Mod(modid = "BrainStoneMod", name = "Brain Stone Mod", version = "v2.34.115 BETA")
+@Mod(modid = "BrainStoneMod", name = "Brain Stone Mod", version = "v2.34.116 BETA")
 @NetworkMod(clientSideRequired = true, serverSideRequired = true, channels = {
 		"BSM", // generic Packet
 		"BSM.TEBBSTS", // TileEntityBlockBrainStoneTrigger Server Packet
