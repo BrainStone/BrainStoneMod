dc899ef13b6ef885d484775a0e546287aed4cdd5
BrainStone <yannick@tedworld.de>
2014-09-08 00:15:44 +0200

    v2.48.3 BETA release
    - Fixed several severe bugs

--- main/java/brainstonemod/BrainStone.java
+++ main/java/brainstonemod/BrainStone.java
@@ -91,7 +91,7 @@ import cpw.mods.fml.relauncher.Side;
 public class BrainStone {
 	public static final String MOD_ID = "BrainStoneMod";
 	public static final String NAME = "Brain Stone Mod";
-	public static final String VERSION = "v2.48.1 BETA release";
+	public static final String VERSION = "v2.48.3 BETA release";
 
 	/** The instance of this mod */
 	@Instance(MOD_ID)
