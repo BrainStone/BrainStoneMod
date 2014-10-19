77ce58f0b25e83f218831a56f37abed0175e4241
HerrKermit <marcelsonntag@alice-dsl.net>
2014-10-19 17:28:26 +0200

    v2.49.121 BETA
    - New texture for the BLS
    
    Conflicts:
    	main/resources/assets/brainstonemod/textures/blocks/brainLightSensor.png

--- main/java/brainstonemod/BrainStone.java
+++ main/java/brainstonemod/BrainStone.java
@@ -91,7 +91,7 @@ import cpw.mods.fml.relauncher.Side;
 public class BrainStone {
 	public static final String MOD_ID = "BrainStoneMod";
 	public static final String NAME = "Brain Stone Mod";
-	public static final String VERSION = "v2.49.120 BETA";
+	public static final String VERSION = "v2.49.121 BETA";
 
 	/** The instance of this mod */
 	@Instance(MOD_ID)
