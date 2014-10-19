af0373a9b9a5f2d08532155cf183b74e6d652ee2
HerrKermit <marcelsonntag@alice-dsl.net>
2014-10-19 17:28:26 +0200

    v2.49.121 BETA
    - New texture for the BLS

--- main/java/brainstonemod/BrainStone.java
+++ main/java/brainstonemod/BrainStone.java
@@ -104,7 +104,7 @@ public class BrainStone {
 	public static final String RESOURCE_PACKAGE = MOD_ID.toLowerCase();
 	public static final String RESOURCE_PREFIX = RESOURCE_PACKAGE + ":";
 	public static final String NAME = "Brain Stone Mod";
-	public static final String VERSION = "v2.49.120 BETA";
+	public static final String VERSION = "v2.49.121 BETA";
 
 	/** The instance of this mod */
 	@Instance(MOD_ID)
