6a59f0c74bfacf6e516250b9025865009e307b9b
The_Fireplace <the.f1repl4ce@gmail.com>
2014-10-22 19:45:51 -0500

    Fixed what Brainstone wanted fixed

--- main/java/brainstonemod/common/helper/NEIBrainstoneConfig.java
+++ main/java/brainstonemod/common/helper/NEIBrainstoneConfig.java
@@ -1,23 +1,28 @@
-package brainstonemod.compat;
+package brainstonemod.common.helper;
 
 import net.minecraft.item.ItemStack;
 import brainstonemod.BrainStone;
 import codechicken.nei.api.API;
 import codechicken.nei.api.IConfigureNEI;
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Optional;
 
+@Optional.Interface(iface = "codechicken.nei.api.API", modid = "NotEnoughItems")
 public class NEIBrainstoneConfig implements IConfigureNEI {
 
+	@Optional.Method(modid = "NotEnoughItems")
 	@Override
 	public String getName() {
-		return BrainStone.class.getAnnotation(Mod.class).name();
+		return BrainStone.NAME;
 	}
 
+	@Optional.Method(modid = "NotEnoughItems")
 	@Override
 	public String getVersion() {
-		return BrainStone.class.getAnnotation(Mod.class).version();
+		return BrainStone.VERSION;
 	}
 
+	@Optional.Method(modid = "NotEnoughItems")
 	@Override
 	public void loadConfig() {
 		API.hideItem(new ItemStack(BrainStone.pulsatingBrainStoneEffect()));
