1b90ae39d700508843c6c1d423abcad721354fcd
BrainStone <yannick@tedworld.de>
2014-10-26 22:05:23 +0100

    v2.49.137 BETA
    - Small improvement in the PulsatinBrainStone
    - Some formatting

--- main/java/brainstonemod/BrainStone.java
+++ main/java/brainstonemod/BrainStone.java
@@ -91,7 +91,7 @@ import cpw.mods.fml.relauncher.Side;
 public class BrainStone {
 	public static final String MOD_ID = "BrainStoneMod";
 	public static final String NAME = "Brain Stone Mod";
-	public static final String VERSION = "v2.48.37 BETA release";
+	public static final String VERSION = "v2.49.137 BETA";
 
 	/** The instance of this mod */
 	@Instance(MOD_ID)
