8a3ef953e127fa640e1c4833289ab53e55e8d0f9
BrainStone <yannick@tedworld.de>
2015-06-10 00:21:40 +0200

    v2.49.259 BETA
    - Added all missing @Optionals

--- main/java/brainstonemod/common/api/thaumcraft/AspectCreator.java
+++ main/java/brainstonemod/common/api/thaumcraft/AspectCreator.java
@@ -10,21 +10,28 @@ import cpw.mods.fml.common.Optional;
 
 @Optional.Interface(iface = "thaumcraft.api.ThaumcraftApi", modid = "Thaumcraft")
 public class AspectCreator {
-	
-	public static final Aspect BRAINSTONE = new Aspect("brainstone",0x56c000,new Aspect[]{Aspect.ENTROPY,Aspect.ENTROPY},new ResourceLocation("thaumcraft","textures/aspects/victus.png"),1);
-	
+	public static final Aspect BRAINSTONE = new Aspect("brainstone", 0x56c000,
+			new Aspect[] { Aspect.ENTROPY, Aspect.ENTROPY },
+			new ResourceLocation("thaumcraft", "textures/aspects/victus.png"),
+			1);
+
+	@Optional.Method(modid = "Thaumcraft")
 	public static void initAspects() {
 		registerBlockAspects();
 	}
-	
+
+	@Optional.Method(modid = "Thaumcraft")
 	private static void registerBlockAspects() {
 		AspectList brainstone = new AspectList();
 		brainstone.aspects.put(BRAINSTONE, 64);
 		brainstone.aspects.put(Aspect.EARTH, 64);
-		ThaumcraftApi.registerObjectTag(new ItemStack(BrainStone.brainStone()), brainstone);
+
+		ThaumcraftApi.registerObjectTag(new ItemStack(BrainStone.brainStone()),
+				brainstone);
 	}
-	
+
+	@Optional.Method(modid = "Thaumcraft")
 	private static void registerItemAspects() {
-		
+
 	}
 }
