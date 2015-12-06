a1fdfd82408efe31326fd420bdb8cf55da26bdb6
lobnews <lobnews@web.de>
2015-12-06 21:01:16 +0100

    + Added Aspects for BrsinStoneblocks and -tools

--- main/java/brainstonemod/common/api/thaumcraft/AspectCreator.java
+++ main/java/brainstonemod/common/api/thaumcraft/AspectCreator.java
@@ -32,10 +32,111 @@ public class AspectCreator {
 
 		ThaumcraftApi.registerObjectTag(new ItemStack(BrainStone.brainStone()),
 				brainstone);
+                
+        brainstone = new AspectList();
+		brainstone.aspects.put(BRAINSTONE, 1);
+		brainstone.aspects.put(Aspect.EARTH, 1);
+		ThaumcraftApi.registerObjectTag(new ItemStack(BrainStone.brainStoneOre()),
+				brainstone);
+                
+        brainstone = new AspectList();
+		brainstone.aspects.put(BRAINSTONE, 5);
+		brainstone.aspects.put(Aspect.EARTH, 5);
+		ThaumcraftApi.registerObjectTag(new ItemStack(BrainStone.dirtyBrainStone()),
+				brainstone);
+                
+        brainstone = new AspectList();
+		brainstone.aspects.put(BRAINSTONE, 10);
+		brainstone.aspects.put(Aspect.MECHANISM, 2);
+		ThaumcraftApi.registerObjectTag(new ItemStack(BrainStone.brainLightSensor()),
+				brainstone);
+                
+        brainstone = new AspectList();
+		brainstone.aspects.put(BRAINSTONE, 10);
+		brainstone.aspects.put(Aspect.MECHANISM, 6);
+		ThaumcraftApi.registerObjectTag(new ItemStack(BrainStone.brainStoneTrigger()),
+				brainstone);
+                
+        brainstone = new AspectList();
+		brainstone.aspects.put(BRAINSTONE, 32);
+		brainstone.aspects.put(Aspect.GREED, 5);
+		brainstone.aspects.put(Aspect.TRAP, 10);
+		ThaumcraftApi.registerObjectTag(new ItemStack(BrainStone.pulsatingBrainStone()),
+				brainstone);
+                 
+        brainstone = new AspectList();
+		brainstone.aspects.put(BRAINSTONE, 8);
+		brainstone.aspects.put(Aspect.MECHANISM, 16);
+		ThaumcraftApi.registerObjectTag(new ItemStack(BrainStone.brainLogicBlock()),
+				brainstone);
 	}
 
 	@Optional.Method(modid = "Thaumcraft")
 	private static void registerItemAspects() {
 
+        AspectList brainstone = new AspectList();
+		brainstone.aspects.put(BRAINSTONE, 1);
+		ThaumcraftApi.registerObjectTag(new ItemStack(BrainStone.brainStoneDust()),
+				brainstone);
+                
+        brainstone = new AspectList();
+		brainstone.aspects.put(BRAINSTONE, 8);
+		brainstone.aspects.put(Aspect.MECHANISM, 10);
+		ThaumcraftApi.registerObjectTag(new ItemStack(BrainStone.brainProcessor()),
+				brainstone);
+                
+        brainstone = new AspectList();
+		brainstone.aspects.put(BRAINSTONE, 8);
+		brainstone.aspects.put(Aspect.WEAPON, 8);
+		ThaumcraftApi.registerObjectTag(new ItemStack(BrainStone.brainStoneSword()),
+				brainstone);
+                
+        brainstone = new AspectList();
+		brainstone.aspects.put(BRAINSTONE, 8);
+		brainstone.aspects.put(Aspect.TOOL, 8);
+		ThaumcraftApi.registerObjectTag(new ItemStack(BrainStone.brainStoneShovel()),
+				brainstone);
+                
+        brainstone = new AspectList();
+		brainstone.aspects.put(BRAINSTONE, 8);
+		brainstone.aspects.put(Aspect.MINE, 8);
+		ThaumcraftApi.registerObjectTag(new ItemStack(BrainStone.brainStonePickaxe()),
+				brainstone);
+                
+        brainstone = new AspectList();
+		brainstone.aspects.put(BRAINSTONE, 8);
+		brainstone.aspects.put(Aspect.TOOL, 8);
+		ThaumcraftApi.registerObjectTag(new ItemStack(BrainStone.brainStoneAxe()),
+				brainstone);
+                
+        brainstone = new AspectList();
+		brainstone.aspects.put(BRAINSTONE, 8);
+		brainstone.aspects.put(Aspect.HARVEST, 8);
+		ThaumcraftApi.registerObjectTag(new ItemStack(BrainStone.brainStoneHoe()),
+				brainstone);
+                
+        brainstone = new AspectList();
+		brainstone.aspects.put(BRAINSTONE, 8);
+		brainstone.aspects.put(Aspect.ARMOR, 8);
+		ThaumcraftApi.registerObjectTag(new ItemStack(BrainStone.brainStoneHelmet()),
+				brainstone);
+                
+        brainstone = new AspectList();
+		brainstone.aspects.put(BRAINSTONE, 8);
+		brainstone.aspects.put(Aspect.ARMOR, 8);
+		ThaumcraftApi.registerObjectTag(new ItemStack(BrainStone.brainStonePlate()),
+				brainstone);
+                
+        brainstone = new AspectList();
+		brainstone.aspects.put(BRAINSTONE, 8);
+		brainstone.aspects.put(Aspect.ARMOR, 8);
+		ThaumcraftApi.registerObjectTag(new ItemStack(BrainStone.brainStoneLeggings()),
+				brainstone);
+                
+        brainstone = new AspectList();
+		brainstone.aspects.put(BRAINSTONE, 8);
+		brainstone.aspects.put(Aspect.ARMOR, 8);
+		ThaumcraftApi.registerObjectTag(new ItemStack(BrainStone.brainStoneBoots()),
+				brainstone);
 	}
 }
