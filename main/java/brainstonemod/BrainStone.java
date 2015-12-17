e20adb7bcfd29ec6ed466d2386df566994ced890
BrainStone <yannick@tedworld.de>
2015-12-17 07:56:57 +0100

    v2.49.483 BETA
    - Made animation for the BrainStoneOre smoother

--- main/java/brainstonemod/BrainStone.java
+++ main/java/brainstonemod/BrainStone.java
@@ -111,7 +111,7 @@ public class BrainStone {
 	public static final String RESOURCE_PACKAGE = MOD_ID.toLowerCase();
 	public static final String RESOURCE_PREFIX = RESOURCE_PACKAGE + ":";
 	public static final String NAME = "Brain Stone Mod";
-	public static final String VERSION = "v2.49.461 BETA";
+	public static final String VERSION = "v2.49.483 BETA";
 	public static final String DEPENDENCIES = "after:EnderIO;after:MineFactoryReloaded;after:Thaumcraft;after:TConstruct";
 
 	/** The instance of this mod */
