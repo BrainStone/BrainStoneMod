3e28c7286466d53adfb1450876605ae360f64716
BrainStone <yannick@tedworld.de>
2016-04-11 05:39:19 +0200

    v2.52.145 BETA
    - Attempt to fix wierd crash

--- main/java/brainstonemod/common/item/ItemBrainStoneLiveCapacitor.java
+++ main/java/brainstonemod/common/item/ItemBrainStoneLiveCapacitor.java
@@ -42,11 +42,7 @@ import net.minecraft.world.World;
 import net.minecraftforge.common.UsernameCache;
 import net.minecraftforge.common.util.Constants.NBT;
 
-@Optional.InterfaceList({
-		// IEnergyContainerItem
-		@Optional.Interface(iface = "cofh.api.energy.IEnergyContainerItem", modid = "CoFHAPI|energy"),
-		// IBauble
-		@Optional.Interface(iface = "baubles.api.IBauble", modid = "Baubles|API") })
+@Optional.Interface(iface = "baubles.api.IBauble", modid = "Baubles|API")
 public class ItemBrainStoneLiveCapacitor extends ItemBrainStoneBase implements IEnergyContainerItem, IBauble {
 	public static final int MaxDamage = 32;
 	public static final int RFperHalfHeart = 1000000;
