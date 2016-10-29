379528e1024b5cfc0844633a2108d57b3537dcba
BrainStone <yannick@tedworld.de>
2016-10-30 00:48:33 +0200

    Rendering DarkSteel Armor Upgrade again

--- src/main/java/brainstonemod/common/api/enderio/BrainStoneUpgrade.java
+++ src/main/java/brainstonemod/common/api/enderio/BrainStoneUpgrade.java
@@ -6,9 +6,11 @@ import crazypants.enderio.item.darksteel.upgrade.AbstractUpgrade;
 import crazypants.enderio.item.darksteel.upgrade.IRenderUpgrade;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.AbstractClientPlayer;
+import net.minecraft.client.model.ModelRenderer;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.block.model.ItemCameraTransforms.TransformType;
 import net.minecraft.client.renderer.entity.RenderPlayer;
-import net.minecraft.client.renderer.texture.TextureMap;
-import net.minecraft.entity.item.EntityItem;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.fml.common.Optional;
@@ -56,97 +58,76 @@ public class BrainStoneUpgrade extends AbstractUpgrade {
 
 	@SideOnly(Side.CLIENT)
 	private class Render implements IRenderUpgrade {
-
-		private EntityItem item = new EntityItem(Minecraft.getMinecraft().theWorld);
-		private final float radToDEgFactor = 57.2958f;
+		private static final float radToDEgFactor = 57.2958f;
+		private final ItemStack brainStone = new ItemStack(BrainStone.brainStone());
 
 		@Override
-		public void doRenderLayer(RenderPlayer renderPlayer, ItemStack stack, AbstractClientPlayer player, float p_177141_2_, float p_177141_3_, float partialTicks, float p_177141_5_, float p_177141_6_, float p_177141_7_, float scale) {
-			/*Item stackItem = stack.getItem();
+		public void doRenderLayer(RenderPlayer renderPlayer, ItemStack stack, AbstractClientPlayer player,
+				float p_177141_2_, float p_177141_3_, float partialTicks, float p_177141_5_, float p_177141_6_,
+				float p_177141_7_, float scale) {
+			Item stackItem = stack.getItem();
+			boolean sneaking = player.isSneaking();
 
-			item.setEntityItemStack(getUpgradeItem());
-			bindItemTexture();
-			glDepthMask(true);
-			item.hoverStart = 0;
+			if (stackItem == DarkSteelItems.itemDarkSteelHelmet) {
+				renderStart(sneaking, renderPlayer.getMainModel().bipedHead);
 
-			if (stackItem == DarkSteelItems.itemDarkSteelHelmet && head) {
-				Helper.translateToHeadLevel(player);
-				GL11.glTranslated(0.27, -0.2375, 0);
-				GL11.glRotated(180, 1, 0, 0);
-				GL11.glScalef(0.5f, 0.5f, 0.5f);
+				GlStateManager.translate(0.0f, -0.45f, -0.27f);
+				GlStateManager.scale(0.3f, 0.3f, 0.3f);
 
-				RenderManager.instance.renderEntityWithPosYaw(item, 0, 0, 0, 0,
-						0);
-			} else if (stackItem == DarkSteelItems.itemDarkSteelChestplate
-					&& !head) {
-				Helper.rotateIfSneaking(player);
-				GL11.glTranslated(0, 0.2, -0.15);
-				GL11.glScalef(0.75f, 0.75f, 0.75f);
+				renderEnd();
+			} else if (stackItem == DarkSteelItems.itemDarkSteelChestplate) {
+				renderStart(sneaking, renderPlayer.getMainModel().bipedBody);
 
-				RenderManager.instance.renderEntityWithPosYaw(item, 0, 0, 0, 0,
-						0);
-			} else if (stackItem == DarkSteelItems.itemDarkSteelLeggings
-					&& !head) {
-				if (player.isSneaking())
-					GL11.glTranslated(0, -0.2, 0.25);
+				GlStateManager.translate(0, 0.3f, -0.15f);
+				GlStateManager.scale(0.5f, 0.5f, 0.5f);
 
-				GL11.glTranslated(0, 0.75, 0);
-				GL11.glRotatef(
-						renderPlayer.getMainModel().bipedRightLeg.rotateAngleX
-								* radToDEgFactor, 1, 0, 0);
-				GL11.glTranslated(-0.25, 0.2, 0);
-				GL11.glScalef(0.5f, 0.5f, 0.5f);
+				renderEnd();
+			} else if (stackItem == DarkSteelItems.itemDarkSteelLeggings) {
+				renderStart(sneaking, renderPlayer.getMainModel().bipedRightLeg);
 
-				RenderManager.instance.renderEntityWithPosYaw(item, 0, 0, 0, 0,
-						0);
+				GlStateManager.translate(0, 0.3f, -0.125f);
+				GlStateManager.scale(0.3f, 0.3f, 0.3f);
 
-				GL11.glScalef(2f, 2f, 2f);
-				GL11.glTranslated(0.25, -0.2, 0);
-				GL11.glRotatef(
-						renderPlayer.getMainModel().bipedRightLeg.rotateAngleX
-								* -radToDEgFactor, 1, 0, 0);
+				renderEnd();
 
-				GL11.glRotatef(
-						renderPlayer.getMainModel().bipedLeftLeg.rotateAngleX
-								* radToDEgFactor, 1, 0, 0);
-				GL11.glTranslated(0.25, 0.2, 0);
-				GL11.glScalef(0.5f, 0.5f, 0.5f);
+				renderStart(sneaking, renderPlayer.getMainModel().bipedLeftLeg);
 
-				RenderManager.instance.renderEntityWithPosYaw(item, 0, 0, 0, 0,
-						0);
-			} else if (stackItem == DarkSteelItems.itemDarkSteelBoots && !head) {
-				if (player.isSneaking())
-					GL11.glTranslated(0, -0.2, 0.25);
+				GlStateManager.translate(0, 0.3f, -0.125f);
+				GlStateManager.scale(0.3f, 0.3f, 0.3f);
 
-				GL11.glTranslated(0, 0.75, 0);
-				GL11.glRotatef(
-						renderPlayer.getMainModel().bipedRightLeg.rotateAngleX
-								* radToDEgFactor, 1, 0, 0);
-				GL11.glTranslated(-0.15, 0.55, -0.175);
-				GL11.glScalef(0.4f, 0.4f, 0.4f);
+				renderEnd();
+			} else if (stackItem == DarkSteelItems.itemDarkSteelBoots) {
+				renderStart(sneaking, renderPlayer.getMainModel().bipedRightLeg);
 
-				RenderManager.instance.renderEntityWithPosYaw(item, 0, 0, 0, 0,
-						0);
+				GlStateManager.translate(-0.15f, 0.55f, 0.0f);
+				GlStateManager.scale(0.3f, 0.3f, 0.3f);
 
-				GL11.glScalef(2.5f, 2.5f, 2.5f);
-				GL11.glTranslated(0.15, -0.55, 0.175);
-				GL11.glRotatef(
-						renderPlayer.getMainModel().bipedRightLeg.rotateAngleX
-								* -radToDEgFactor, 1, 0, 0);
+				renderEnd();
 
-				GL11.glRotatef(
-						renderPlayer.getMainModel().bipedLeftLeg.rotateAngleX
-								* radToDEgFactor, 1, 0, 0);
-				GL11.glTranslated(0.15, 0.55, -0.175);
-				GL11.glScalef(0.4f, 0.4f, 0.4f);
+				renderStart(sneaking, renderPlayer.getMainModel().bipedLeftLeg);
 
-				RenderManager.instance.renderEntityWithPosYaw(item, 0, 0, 0, 0,
-						0);
-			}*/
+				GlStateManager.translate(0.15f, 0.55f, 0.0f);
+				GlStateManager.scale(0.3f, 0.3f, 0.3f);
+
+				renderEnd();
+			}
 		}
-	}
 
-	public static void bindItemTexture() {
-		Minecraft.getMinecraft().getTextureManager().bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
+		private void renderStart(boolean sneaking, ModelRenderer modelRenderer) {
+			GlStateManager.pushMatrix();
+
+			if (sneaking) {
+				GlStateManager.translate(0.0F, 0.2F, 0.0F);
+			}
+
+			modelRenderer.postRender(0.0625F);
+			GlStateManager.color(1.0f, 1.0f, 1.0f, 1.0f);
+		}
+
+		private void renderEnd() {
+			Minecraft.getMinecraft().getRenderItem().renderItem(brainStone, TransformType.FIXED);
+
+			GlStateManager.popMatrix();
+		}
 	}
 }
