115a812a35feb409fa9fd56df90186071df91b43
BrainStone <yannick@tedworld.de>
2016-10-31 19:50:53 +0100

    Fixed EnderIO crash
    - Also fixed crash caused by lombok.config files not being read

--- src/main/java/brainstonemod/common/api/enderio/EnderIOItems.java
+++ src/main/java/brainstonemod/common/api/enderio/EnderIOItems.java
@@ -1,6 +1,6 @@
 package brainstonemod.common.api.enderio;
 
-import crazypants.enderio.EnderIO;
+import crazypants.enderio.ModObject;
 import net.minecraft.item.ItemStack;
 
 public final class EnderIOItems {
@@ -8,14 +8,14 @@ public final class EnderIOItems {
 	}
 
 	public static final ItemStack getSentientEnder() {
-		return new ItemStack(EnderIO.itemFrankenSkull, 1, 4);
+		return new ItemStack(ModObject.itemFrankenSkull.getItem(), 1, 4);
 	}
 
 	public static final ItemStack getXPRod() {
-		return new ItemStack(EnderIO.itemXpTransfer, 1, 0);
+		return new ItemStack(ModObject.itemXpTransfer.getItem(), 1, 0);
 	}
 
 	public static final ItemStack getOctadicCapacitor() {
-		return new ItemStack(EnderIO.itemBasicCapacitor, 1, 2);
+		return new ItemStack(ModObject.itemBasicCapacitor.getItem(), 1, 2);
 	}
 }
