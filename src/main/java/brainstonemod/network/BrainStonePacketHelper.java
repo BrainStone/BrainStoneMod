771fda4196d40357ed83f9b63b642bb192eb4885
BrainStone <yannick@tedworld.de>
2016-11-03 02:12:58 +0100

    Attempt at fixing #15

--- src/main/java/brainstonemod/network/BrainStonePacketHelper.java
+++ src/main/java/brainstonemod/network/BrainStonePacketHelper.java
@@ -8,14 +8,12 @@ import brainstonemod.network.packet.clientbound.PacketSmokeParticle;
 import brainstonemod.network.packet.clientbound.PacketTriggerMobs;
 import io.netty.channel.ChannelHandler.Sharable;
 import lombok.experimental.UtilityClass;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.network.Packet;
 import net.minecraft.network.play.server.SPacketEntityVelocity;
-import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 
 @Sharable
 @UtilityClass
@@ -32,37 +30,11 @@ public class BrainStonePacketHelper {
 				(EntityPlayerMP) player);
 	}
 
-	/**
-	 * Sends a sync packet to the closest players.
-	 * 
-	 * @param x
-	 *            x-Coordinate
-	 * @param y
-	 *            y-Coordinate
-	 * @param z
-	 *            z-Coordinate
-	 * @param world
-	 *            The world. Needed for the dimension id
-	 * @param packet
-	 *            The packet to send
-	 */
-	public static void sendPacketToClosestPlayers(int x, int y, int z, World world, final Packet<?> packet) {
-		final int radius = 256;
-
-		for (Entity pl : world.getEntitiesWithinAABBExcludingEntity(null,
-				new AxisAlignedBB(x - radius, y - radius, z - radius, x + radius, y + radius, z + radius))) {
-			if (pl instanceof EntityPlayerMP) {
-				((EntityPlayerMP) pl).connection.sendPacket(packet);
-			}
-		}
-
-	}
-
 	// DOCME
 	public static void sendPlayerUpdateMovementPacket(EntityPlayer entity, double x, double y, double z) {
 		final SPacketEntityVelocity packet = new SPacketEntityVelocity(entity.getEntityId(), x, y, z);
 
-		sendPacketToClosestPlayers((int) entity.posX, (int) entity.posY, (int) entity.posZ, entity.worldObj, packet);
+		PacketDispatcher.sendToAll((IMessage) packet);
 	}
 
 	// DOCME
