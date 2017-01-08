aa485d66ea9d70171301cc83769b6f71f5e5edf3
BrainStone <yannick@tedworld.de>
2017-01-08 03:26:14 +0100

    Simplified Essence Of Life effect code

--- src/main/java/brainstonemod/common/item/ItemEssenceOfLife.java
+++ src/main/java/brainstonemod/common/item/ItemEssenceOfLife.java
@@ -1,19 +1,20 @@
 package brainstonemod.common.item;
 
 import brainstonemod.BrainStone;
-import brainstonemod.common.item.template.ItemBrainStoneFood;
+import lombok.AllArgsConstructor;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.MobEffects;
 import net.minecraft.item.EnumRarity;
+import net.minecraft.item.ItemFood;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.world.World;
 
-public class ItemEssenceOfLife extends ItemBrainStoneFood {
+public class ItemEssenceOfLife extends ItemFood {
 	public ItemEssenceOfLife() {
-		super(20, 10.0F);
+		super(20, 10.0F, false);
 
 		setAlwaysEdible();
 		setCreativeTab(BrainStone.getCreativeTab(CreativeTabs.MATERIALS));
@@ -27,33 +28,38 @@ public class ItemEssenceOfLife extends ItemBrainStoneFood {
 	@Override
 	protected void onFoodEaten(ItemStack stack, World world, EntityPlayer player) {
 		if (!world.isRemote) {
-			int duration;
-			PotionEffect potion;
-			Object[][] potionEffects = new Object[][] {
+			PotionInfo[] potionEffects = new PotionInfo[] {
 					// Absorption X for 120s
-					{ MobEffects.ABSORPTION, 120, 9 },
-					// Resitance III for 120s
-					{ MobEffects.RESISTANCE, 120, 2 },
+					new PotionInfo(MobEffects.ABSORPTION, 120, 10),
+					// Resistance III for 120s
+					new PotionInfo(MobEffects.RESISTANCE, 120, 3),
 					// Strength III for 120s
-					{ MobEffects.STRENGTH, 120, 2 },
+					new PotionInfo(MobEffects.STRENGTH, 120, 3),
 					// Regeneration III for 30s
-					{ MobEffects.REGENERATION, 30, 2 },
+					new PotionInfo(MobEffects.REGENERATION, 30, 3),
 					// Fire Resistance for 300s
-					{ MobEffects.FIRE_RESISTANCE, 300, 0 },
+					new PotionInfo(MobEffects.FIRE_RESISTANCE, 300),
 					// Water Breathing for 300s
-					{ MobEffects.WATER_BREATHING, 300, 0 } };
+					new PotionInfo(MobEffects.WATER_BREATHING, 300) };
 
-			for (Object[] potionToAdd : potionEffects) {
-				duration = 0;
-				potion = player.getActivePotionEffect((Potion) potionToAdd[0]);
-
-				if (potion != null) {
-					duration = potion.getDuration();
-				}
-
-				player.addPotionEffect(new PotionEffect(((Potion) potionToAdd[0]),
-						Math.max((Integer) potionToAdd[1] * 20, duration), (Integer) potionToAdd[2]));
+			for (PotionInfo potionToAdd : potionEffects) {
+				player.addPotionEffect(potionToAdd.toPotionEffect());
 			}
+		}
+	}
+
+	@AllArgsConstructor
+	private class PotionInfo {
+		private final Potion potionEffect;
+		private final int duration;
+		private final int level;
+
+		public PotionInfo(Potion potionEffect, int duration) {
+			this(potionEffect, duration, 1);
+		}
+
+		public PotionEffect toPotionEffect() {
+			return new PotionEffect(potionEffect, duration * 20, level - 1);
 		}
 	}
 }
