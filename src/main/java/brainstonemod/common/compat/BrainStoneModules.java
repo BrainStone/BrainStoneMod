134ceee9c80a39f65d481d00199ab725cc352c08
BrainStone <yannick@tedworld.de>
2017-08-25 01:13:20 +0200

    Simplified Module checks and added ThermalExpansionCompat

--- src/main/java/brainstonemod/common/compat/BrainStoneModules.java
+++ src/main/java/brainstonemod/common/compat/BrainStoneModules.java
@@ -11,6 +11,7 @@ import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 import brainstonemod.common.compat.betterachievements.BetterAchievementsCompat;
+import brainstonemod.common.compat.cofh.thermalexpansion.ThermalExpansionCompat;
 import brainstonemod.common.compat.forestry.ForestryCompat;
 import brainstonemod.common.compat.immersiveengineering.ImmersiveEngineeringCompat;
 import brainstonemod.common.compat.jeresources.JEResourcesCompat;
@@ -40,13 +41,15 @@ public final class BrainStoneModules {
 	public static final String OVERLORD_MODID = "overlord";
 	public static final String TESLA_MODID = "tesla";
 	public static final String THAUMCRAFT_MODID = "thaumcraft";
+	public static final String THERMAL_EXPANSION_MODID = "thermalexpansion";
 	public static final String TINKERS_CONSTRUCT_MODID = "tconstruct";
 
 	public static final String DEPENDENCIES = "after:" + BAUBLES_MODID + ";after:" + BETTER_ACHIEVEMENTS_MODID
 			+ ";after:" + CTM_MODID + ";after:" + DRACONIC_EVOLUTION_MODID + ";after:" + ENDER_IO_MODID + ";after:"
 			+ EX_NIHILO_ADSCENSIO_MODID + ";after:" + FORESTRY_MODID + ";after:" + IMMERSIVE_ENGINEERING_MODID
 			+ ";after:" + JEI_MODID + ";after:" + JER_MODID + ";after:" + MFR_MODID + ";after:" + OVERLORD_MODID
-			+ ";after:" + TESLA_MODID + ";after:" + THAUMCRAFT_MODID + ";after:" + TINKERS_CONSTRUCT_MODID;
+			+ ";after:" + TESLA_MODID + ";after:" + THAUMCRAFT_MODID + ";after:" + THERMAL_EXPANSION_MODID + ";after:"
+			+ TINKERS_CONSTRUCT_MODID;
 
 	private static Boolean BAUBLES;
 	private static Boolean BETTER_ACHIEVEMENTS;
@@ -61,152 +64,108 @@ public final class BrainStoneModules {
 	private static Boolean MFR;
 	private static Boolean OVERLORD;
 	private static Boolean THAUMCRAFT;
+	private static Boolean THERMAL_EXPANSION;
 	private static Boolean TINKERS_CONSTRUCT;
 	private static Boolean TESLA;
 
 	@Getter
 	private static final Set<ModuleInformation> allModules = detectAllModules();
 	@Getter
-	private static Set<ModuleInformation> activeModules = new TreeSet<>();
+	private static final Set<ModuleInformation> activeModules = new TreeSet<>();
 
 	@Module(modid = BAUBLES_MODID, name = "Baubles", message = "Enabling brainStoneLiveCapacitor in the BELT slot.")
 	public static boolean baubles() {
-		if (BAUBLES == null) {
-			BAUBLES = Loader.isModLoaded(BAUBLES_MODID);
-		}
-
-		return BAUBLES;
+		return testMod(BAUBLES_MODID, BAUBLES);
 	}
 
 	@Module(modid = BETTER_ACHIEVEMENTS_MODID, name = "Better Achievements", message = "Setting custom achievement icon.", integration = BetterAchievementsCompat.class)
 	public static boolean betterAchievements() {
-		if (BETTER_ACHIEVEMENTS == null) {
-			BETTER_ACHIEVEMENTS = Loader.isModLoaded(BETTER_ACHIEVEMENTS_MODID);
-		}
-
-		return BETTER_ACHIEVEMENTS;
+		return testMod(BETTER_ACHIEVEMENTS_MODID, BETTER_ACHIEVEMENTS);
 	}
 
 	@Module(modid = CTM_MODID, name = "Connected Textures Mod", message = "Using connected Textures.")
 	public static boolean CTM() {
-		if (CTM == null) {
-			CTM = Loader.isModLoaded(CTM_MODID);
-		}
-
-		return CTM;
+		return testMod(CTM_MODID, CTM);
 	}
 
 	@Module(modid = DRACONIC_EVOLUTION_MODID, name = "Draconic Evolution", message = "Balancing and adding recipes.")
 	public static boolean draconicEvolution() {
-		if (DRACONIC_EVOLUTION == null) {
-			DRACONIC_EVOLUTION = Loader.isModLoaded(DRACONIC_EVOLUTION_MODID);
-		}
-
-		return DRACONIC_EVOLUTION;
+		return testMod(DRACONIC_EVOLUTION_MODID, DRACONIC_EVOLUTION);
 	}
 
 	// @Module(modid = ENDER_IO_MODID, name = "Ender IO", message = "Enabling
 	// recipes and armor upgrade.", integration = EnderIOCompat.class)
 	protected static boolean enderIO() {
-		if (ENDER_IO == null) {
-			ENDER_IO = Loader.isModLoaded(ENDER_IO_MODID);
-		}
-
-		return ENDER_IO;
+		return testMod(ENDER_IO_MODID, ENDER_IO);
 	}
 
 	// @Module(modid = EX_NIHILO_ADSCENSIO_MODID, name = "Ex Nihilo Adscensio",
 	// message = "Making Brain Stone siftable.", integration =
 	// ExNihiloAdscensioCompat.class)
 	protected static boolean exNihiloAdscensio() {
-		if (EX_NIHILO_ADSCENSIO == null) {
-			EX_NIHILO_ADSCENSIO = Loader.isModLoaded(EX_NIHILO_ADSCENSIO_MODID);
-		}
-
-		return EX_NIHILO_ADSCENSIO;
+		return testMod(EX_NIHILO_ADSCENSIO_MODID, EX_NIHILO_ADSCENSIO);
 	}
 
 	@Module(modid = FORESTRY_MODID, name = "Forestry", message = "Adding bees and related items.", integration = ForestryCompat.class)
 	public static boolean forestry() {
-		if (FORESTRY == null) {
-			FORESTRY = Loader.isModLoaded(FORESTRY_MODID);
-		}
-
-		return FORESTRY;
+		return testMod(FORESTRY_MODID, FORESTRY);
 	}
 
 	@Module(modid = IMMERSIVE_ENGINEERING_MODID, name = "Immersive Engineering", message = "Adding recipes.", integration = ImmersiveEngineeringCompat.class)
 	public static boolean immersiveEngineering() {
-		if (IMMERSIVE_ENGINEERING == null) {
-			IMMERSIVE_ENGINEERING = Loader.isModLoaded(IMMERSIVE_ENGINEERING_MODID);
-		}
-
-		return IMMERSIVE_ENGINEERING;
+		return testMod(IMMERSIVE_ENGINEERING_MODID, IMMERSIVE_ENGINEERING);
 	}
 
 	@Module(modid = JEI_MODID, name = "Just Enough Items (JEI)", message = "Adding recipes to JEI.")
 	public static boolean JEI() {
-		if (JEI == null) {
-			JEI = Loader.isModLoaded(JEI_MODID);
-		}
-
-		return JEI;
+		return testMod(JEI_MODID, JEI);
 	}
 
 	@Module(modid = JER_MODID, name = "Just Enough Resources (JER)", message = "Adding drop and orgen information.", integration = JEResourcesCompat.class)
 	public static boolean JER() {
-		if (JER == null) {
-			JER = Loader.isModLoaded(JER_MODID);
-		}
-
-		return JER;
+		return testMod(JER_MODID, JER);
 	}
 
 	// @Module(modid = MFR_MODID, name = "MineFactory Reloaded", message =
 	// "Adding BrainStoneOre to the MiningLaser.")
 	protected static boolean MFR() {
-		if (MFR == null) {
-			MFR = Loader.isModLoaded(MFR_MODID);
-		}
-
-		return MFR;
+		return testMod(MFR_MODID, MFR);
 	}
 
 	@Module(modid = OVERLORD_MODID, name = "Overlord", message = "Adding Pulsating BrainStone Augment.", integration = OverlordCompat.class)
 	public static boolean overlord() {
-		if (OVERLORD == null) {
-			OVERLORD = Loader.isModLoaded(OVERLORD_MODID);
-		}
-
-		return OVERLORD;
+		return testMod(OVERLORD_MODID, OVERLORD);
 	}
 
 	// @Module(modid = THAUMCRAFT_MODID, name = "Thaumcraft", message = "Adding
 	// aspect and fitting aspects to items.")
-	public static boolean thaumcraft() {
-		if (THAUMCRAFT == null) {
-			THAUMCRAFT = Loader.isModLoaded(THAUMCRAFT_MODID);
-		}
+	protected static boolean thaumcraft() {
+		return testMod(THAUMCRAFT_MODID, THAUMCRAFT);
+	}
 
-		return THAUMCRAFT;
+	@Module(modid = THERMAL_EXPANSION_MODID, name = "Thermal Expansion", message = "Adding recipes.", integration = ThermalExpansionCompat.class)
+	public static boolean thermalExpansion() {
+		return testMod(THERMAL_EXPANSION_MODID, THERMAL_EXPANSION);
 	}
 
 	@Module(modid = TINKERS_CONSTRUCT_MODID, name = "Tinker's Construct", message = "Registering BrainStone materials as tool materials.", integration = TiConCompat.class)
 	public static boolean tinkersConstruct() {
-		if (TINKERS_CONSTRUCT == null) {
-			TINKERS_CONSTRUCT = Loader.isModLoaded(TINKERS_CONSTRUCT_MODID);
-		}
-
-		return TINKERS_CONSTRUCT;
+		return testMod(TINKERS_CONSTRUCT_MODID, TINKERS_CONSTRUCT);
 	}
 
 	@Module(modid = TESLA_MODID, name = "TESLA", message = "Enabling support for TESLA energy API.")
 	public static boolean tesla() {
-		if (TESLA == null) {
-			TESLA = Loader.isModLoaded(TESLA_MODID);
-		}
+		return testMod(TESLA_MODID, TESLA);
+	}
 
-		return TESLA;
+	private static boolean testMod(String modID, Boolean storage) {
+		synchronized (storage) {
+			if (storage == null) {
+				storage = Loader.isModLoaded(modID);
+			}
+
+			return storage;
+		}
 	}
 
 	private static Set<ModuleInformation> detectAllModules() {
---
+++ src/main/java/brainstonemod/common/compat/cofh/thermalexpansion/ThermalExpansionCompat.java
@@ -0,0 +1,28 @@
+package brainstonemod.common.compat.cofh.thermalexpansion;
+
+import brainstonemod.common.compat.IModIntegration;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+public class ThermalExpansionCompat implements IModIntegration {
+	@Override
+	public void preInit(FMLPreInitializationEvent event) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void init(FMLInitializationEvent event) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void postInit(FMLPostInitializationEvent event) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void addAchievement() {
+		// TODO Auto-generated method stub
+	}
+}
