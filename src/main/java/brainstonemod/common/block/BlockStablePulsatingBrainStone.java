ba744662247f5b2d1700dab00fb9c585131da2ee
BrainStone <yannick@tedworld.de>
2017-09-13 19:09:29 +0200

    Made Stable Pulsating Brain Stone Ender Dragon and Wither proof

---
+++ src/main/java/brainstonemod/common/block/BlockStablePulsatingBrainStone.java
@@ -0,0 +1,32 @@
+package brainstonemod.common.block;
+
+import brainstonemod.BrainStone;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.boss.EntityDragon;
+import net.minecraft.entity.boss.EntityWither;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+
+public class BlockStablePulsatingBrainStone extends Block {
+	public BlockStablePulsatingBrainStone() {
+		super(Material.ROCK);
+
+		setHardness(4.0F);
+		setResistance(1.5F);
+		setLightLevel(1.0F);
+		setCreativeTab(BrainStone.getCreativeTab(CreativeTabs.BUILDING_BLOCKS));
+		setHarvestLevel("pickaxe", 4);
+
+		blockParticleGravity = -0.2F;
+	}
+
+	@Override
+	public boolean canEntityDestroy(IBlockState state, IBlockAccess world, BlockPos pos, Entity entity) {
+		return !((entity instanceof EntityDragon) || (entity instanceof EntityWither))
+				&& super.canEntityDestroy(state, world, pos, entity);
+	}
+}
